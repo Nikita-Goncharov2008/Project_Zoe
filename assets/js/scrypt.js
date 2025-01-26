af9e8b59eac872eeab86c9ef0dd6a9362f5e50ca
Nikita Goncharov <nikitagoncharov993@gmail.com>
2025-01-26 17:38:48 -0500

    first commit

---
+++ assets/js/scrypt.js
@@ -0,0 +1,10 @@
+document.querySelector('.burger').onclick = ()=>{
+    // if(document.querySelector('.header__menu').classList.contains('mobile__burger')){
+    //     document.querySelector('.header__menu').classList.remove('mobile__burger')
+    // }else{
+    //     document.querySelector('.header__menu').classList.add('mobile__burger')
+    // }
+
+    document.querySelector('.header__menu').classList.toggle('mobile__burger')
+
+}
